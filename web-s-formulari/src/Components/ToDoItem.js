e41a2cb3ae784939d2c9bc118a28729b21f18cb2
Fiser04 <vf6a@seznam.cz>
2026-03-14 17:24:30 +0100

    Update ToDoItem.js

--- web-s-formulari/src/Components/ToDoItem.js
+++ web-s-formulari/src/Components/ToDoItem.js
@@ -1,11 +1,36 @@
 import React from "react";
 
 class ToDoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: this.props.completed,
+    };
+  }
+  changeCompleted = (event) => {
+    this.setState({ completed: event.target.checked });
+    alert("You clicked on " + this.props.title);
+  };
+  // alertUser = (event) => {
+  //   alert("You clicked on " + this.props.title);
+  // };
   render() {
+    let changed = "";
+    if (this.state.completed !== this.props.completed) {
+      changed = " (changed)";
+    }
     return (
       <li>
-        {this.props.title}{" "}
-        <input type="checkbox" checked={this.props.completed} />;
+        {/* <form onSubmit={this.alertUser}>*/}
+        <form>
+          {this.props.title}{" "}
+          <input
+            type="checkbox"
+            checked={this.state.completed}
+            onChange={this.changeCompleted}
+          />
+          {changed}
+        </form>
       </li>
     );
   }
