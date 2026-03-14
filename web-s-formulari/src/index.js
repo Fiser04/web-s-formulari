fd059bb04b54279bf5eba659e374cfdec04cef99
Fiser04 <vf6a@seznam.cz>
2026-03-14 16:59:49 +0100

    Update index.js

--- web-s-formulari/src/index.js
+++ web-s-formulari/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ToDoContainer from "./components/ToDoContainer";
+import ToDoContainer from "./Components/TodoContainer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
