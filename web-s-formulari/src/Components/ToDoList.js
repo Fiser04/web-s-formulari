a45e789b75f7edeefa756482ed9efc742578d121
Fiser04 <vf6a@seznam.cz>
2026-03-29 23:14:51 +0200

    Update ToDoList.js

--- web-s-formulari/src/Components/ToDoList.js
+++ web-s-formulari/src/Components/ToDoList.js
@@ -1,23 +1,18 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
-
 class ToDoList extends React.Component {
   render() {
     return (
-      <div className="form-container">
-        <ul>
-          {this.props.items.map((item) => (
-            <ToDoItem
-              key={item.id}
-              item={item}
-              title={item.title}
-              completed={item.completed}
-              handleChangeProps={this.props.handleChangeProps}
-              deleteItemProps={this.props.deleteItemProps}
-            />
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.props.items.map((item) => (
+          <ToDoItem
+            key={item.id}
+            item={item}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteItemProps={this.props.deleteItemProps}
+          />
+        ))}
+      </ul>
     );
   }
 }
