3f2f78800669d5a5e1a28eb7621bf196fa46809d
Fiser04 <vf6a@seznam.cz>
2026-03-29 23:16:40 +0200

    Update index.js

--- web-s-formulari/src/index.js
+++ web-s-formulari/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import ToDoContainer from "./Components/TodoContainer";
 import "./App.css";
