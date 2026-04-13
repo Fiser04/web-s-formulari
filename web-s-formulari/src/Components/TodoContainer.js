0d8920cf33372a3f55932fa50d166509a173ef1d
Fiser04 <vf6a@seznam.cz>
2026-04-13 10:33:11 +0200

    Load and save todos to localStorage

--- web-s-formulari/src/Components/TodoContainer.js
+++ web-s-formulari/src/Components/TodoContainer.js
@@ -6,45 +6,7 @@ import Header from "./Header";
 
 class ToDoContainer extends React.Component {
   state = {
-    items: [
-      {
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "quis ut nam facilis et officia qui",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: "fugiat veniam minus",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 4,
-        title: "et porro tempora",
-        completed: true,
-      },
-      {
-        userId: 1,
-        id: 5,
-        title:
-          "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 6,
-        title: "qui ullam ratione quibusdam voluptatem quia omnis",
-        completed: false,
-      },
-    ],
+    items: [],
   };
   //HANDLE CHANGE STATE UPDATE
   handleChange2 = (id) => {
@@ -118,6 +80,28 @@ class ToDoContainer extends React.Component {
     });
   };
 
+  componentDidMount() {
+    // async call to fetch data from API and update state with the response
+    // fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    //   .then((response) => response.json())
+    //   .then((data) => this.setState({ items: data }));
+
+    const temp = localStorage.getItem("items");
+    const loadedItems = JSON.parse(temp);
+    if (loadedItems) {
+      this.setState({
+        items: loadedItems,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("component updated");
+    if (prevState.items !== this.state.items) {
+      console.log("items updated");
+      localStorage.setItem("items", JSON.stringify(this.state.items));
+    }
+  }
   render() {
     return (
       <div className="container">
