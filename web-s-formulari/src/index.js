55b636464029bf80a86c2b4b8608481dd4af45ea
Fiser04 <vf6a@seznam.cz>
2026-04-20 10:18:22 +0200

    Remove old ToDo class components and update import
    Point index.js to the new function-based container at
    functionBased/Components/TodoContainer

--- web-s-formulari/src/index.js
+++ web-s-formulari/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import ToDoContainer from "./Components/TodoContainer";
+import ToDoContainer from "./functionBased/Components//TodoContainer";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
